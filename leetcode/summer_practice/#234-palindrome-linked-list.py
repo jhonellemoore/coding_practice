d84280fdabb6deec32e7824675b59b5be53991cf
Jhonelle Moore <124172687+jhonellemoore@users.noreply.github.com>
2024-07-07 23:57:52 -0400

    Create #234-palindrome-linked-list.py

---
+++ leetcode/summer_practice/#234-palindrome-linked-list.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+""""
+Algorithm:
+- Move the fast and slow pointers until the slow pointer gets to the middle
+- When the slow pointer gets to the middle, call the reverse function
+    - reverse function reverses from middle to end
+
+"""
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        def reverseList(middle):
+            prev = middle
+            curr = middle.next
+            while curr != None:
+                next_node = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_node    
+            return prev
+
+        # Goal = we want the slow pointer to end up in the middle of the list
+        slow = head
+        fast = head
+        if head is None:
+            return True
+        while fast.next != None and fast.next.next != None :
+            fast = fast.next.next
+            slow = slow.next
+
+        # reverse from middle to end
+        new_end = reverseList(slow)
+        
+        while head <= new_end:
+            if head.val != new_end.val:
+                return False
+            head = head.next
+            new_end = new_end.next
+        
+        return True
